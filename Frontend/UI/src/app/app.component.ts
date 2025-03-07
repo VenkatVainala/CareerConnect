33b508380408651085fa743c439dc1e4d8abcd3a
Venkat Sai <vainalavenkatsai@gmail.com>
2025-03-06 19:39:55 -0500

    Create Login Component

--- Frontend/UI/src/app/app.component.ts
+++ Frontend/UI/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; // Import RouterOutlet here
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule, RouterOutlet],  // Add RouterOutlet to imports
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
